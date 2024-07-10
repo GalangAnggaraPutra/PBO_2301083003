57381a6a8de7ac21277b721e74a028458c76e843
Galang Anggara Putra <galanganggra93@gmail.com>
2024-07-10 16:20:55 +0700

    Tugas PBO Galang Anggara Putra 2301083003 TK 1A

---
+++ Pratikum_18_05_2024/IOExceptions.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pratikum_18_05_2024;
+
+/**
+ *
+ * @author Galang Anggara Putra
+ */
+class IOExceptions extends Exception {
+    
+}
